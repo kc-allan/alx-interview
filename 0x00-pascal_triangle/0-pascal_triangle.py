beba824b385849d8baa98acf025db9a9ffa8b301
Allan <farmtrace.co.ke>
2024-10-22 14:31:51 +0300

    Pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+A function that returns a list of lists of integers representing the Pascal’s triangle of n
+"""
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     triangle = [[1]]
     if n <= 0:
         return triangle
