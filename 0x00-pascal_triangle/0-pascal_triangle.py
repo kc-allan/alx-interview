4c14e53cbffd166a4545fff4366a2af89de19ce5
Allan <farmtrace.co.ke>
2024-10-22 14:21:46 +0300

    Pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    triangle = [[1]]
+    if n <= 0:
+        return triangle
+    for k in range(n-1):
+        prev = triangle[-1]
+        col = []
+        for index, val in enumerate(prev):
+            if index-1 < 0:
+                col.append(val + 0)
+            if index+1 >= len(prev):
+                col.append(val+0)
+            else:
+                col.append(val + prev[index+1])
+        triangle.append(col)
+    return triangle
