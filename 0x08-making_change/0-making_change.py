5a19edaacf7ac7fed08e987f80e97b9ece11f494
kc-allan <allancheruiyot003@gmail.com>
2025-01-20 22:31:02 +0300

    pep8 --first 0x08-making_change/0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,7 +6,9 @@ Finding the minimum number of coins need to make change
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+    Given a pile of coins of different values,\
+        determine the fewest number of coins\
+        needed to meet a given amount total.
     :param coins: a list of the values of the coins in your possession
     :param total: the total amount of money you need to make change for
     :return: the fewest number of coins needed to meet the total
